fe307396d84a7c58dbe32297bef4ebe4f7c20298
SirisakVongsawat <122779588+SirisakVongsawat@users.noreply.github.com>
2023-09-04 08:35:50 +0000

    6531503084_summit

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -8,21 +8,49 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//TODO: add webservlet to "/calbmi"
-public class BMICalculatorServlet extends HttpServlet{
+@WebServlet("/calbmi") // Add the annotation to specify the servlet mapping
+public class BMICalculatorServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO: get parameter from request: "weight" and "height"
-        
-        //TODO: calculate bmi
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-        //TODO: determine the built from BMI
-      
-        //TODO: add bmi and built to the request's attribute
+        double weight = Double.parseDouble(request.getParameter("weight"));
+        double height = Double.parseDouble(request.getParameter("height"));
 
-        //TODO: forward to jsp
-           
-    }
     
+
+        double bmi = calculateBMI(weight, height);
+
+
+
+        String bodyType = determineBodyType(bmi);
+
+    
+
+        request.setAttribute("bmi", Math.round(bmi));
+        request.setAttribute("bodyType", bodyType);
+
+
+        request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
+    }
+
+    // Calculate BMI method
+    private double calculateBMI(double weight, double height) {
+        // BMI formula: BMI = weight(kg) / (height(m) * height(m))
+        return weight / (height * height);
+    }
+
+    // Determine body type method (a simple example)
+    private String determineBodyType(double bmi) {
+        if (bmi < 18.5) {
+            return "underweight";
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+            return "normal weight";
+        } else if (bmi >= 24.9 && bmi < 29.9) {
+            return "overweight";
+        } else {
+            return "obese";
+        }
+    }
 }
